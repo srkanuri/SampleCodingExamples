482b9500bba298f0a0ab8fa3fa9df98052be6124
Srikanth Kanuri <srkanuri@ui.edu>
2017-04-18 20:22:42 -0400

    Added Test Enum

---
+++ src/me/srikanthkanuri/beginner/TestEnum.java
@@ -0,0 +1,18 @@
+package me.srikanthkanuri.beginner;
+
+public class TestEnum {
+	private enum Days{
+		SUNDAY,
+		MONDAY,
+		TUESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY
+	}
+	public static void main(String[] args) {
+		for(Days val : Days.values()) {
+			
+		}
+	}
+
+}
